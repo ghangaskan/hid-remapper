a31fcbaa69bf9442c04699875fde642f0b5ddea7
ghangaskan <72000925+ghangaskan@users.noreply.github.com>
2023-09-03 17:26:26 -0700

    updated another thing

--- firmware/src/gpio_write.h
+++ firmware/src/gpio_write.h
@@ -1,9 +1,9 @@
 #ifndef _GPIO_WRITE_H_
 #define _GPIO_WRITE_H_
 
-void gpio_write_on_mask(uint32_t PinMask);
-void gpio_write_off_mask_maybe(uint32_t PinMask);
-void gpio_init_read_mask(uint gpio_mask);
-void gpio_init_write_mask(uint gpio_mask);
+void gpio_write_on_mask(int PinMask);
+void gpio_write_off_mask_maybe(int PinMask);
+void gpio_init_read_mask(int gpio_mask);
+void gpio_init_write_mask(int gpio_mask);
 
 #endif
